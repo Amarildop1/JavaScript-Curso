e6eae36b318f0fd1199d83edc1dddba6c43caf7d
Amarildo <amarildop1dev@gmail.com>
2020-05-14 21:12:35 -0300

    script

---
+++ Exercicio-A26prof/js/script.js
@@ -0,0 +1,11 @@
+const form = document.querySelector('.formulario');
+const resultado = document.querySelector('.resultado');
+
+function recebeEventoForm(evento){
+    evento.preventDefault();
+
+    
+}
+
+
+form.addEventListener('submit', recebeEventoForm);
