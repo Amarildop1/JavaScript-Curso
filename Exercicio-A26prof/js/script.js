73c04dfafb5ff1a6026190c7585b97b8e7e62367
Amarildo <amarildop1dev@gmail.com>
2020-07-23 22:13:18 -0300

    script funcionando

--- Exercicio-A26prof/js/script.js
+++ Exercicio-A26prof/js/script.js
@@ -1,11 +1,22 @@
 const form = document.querySelector('.formulario');
 const resultado = document.querySelector('.resultado');
 
+let pesoInput = document.querySelector('#peso');
+let alturaInput = document.querySelector('#altura');
+
+
 function recebeEventoForm(evento){
     evento.preventDefault();
 
+    let peso = parseFloat(pesoInput.value);
+    let altura = parseFloat(alturaInput.value);
+
+    let IMC = peso / (altura**2 );
+
+    resultado.innerHTML += `<strong>IMC:</strong> ${IMC.toFixed(2)} <br>`;
     
 }
 
 
 form.addEventListener('submit', recebeEventoForm);
+
