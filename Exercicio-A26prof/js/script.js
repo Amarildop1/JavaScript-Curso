5a8906ad20a54f4b260ff5b609ad0aa571d30b9f
Amarildo <amarildop1dev@gmail.com>
2020-08-13 23:51:49 -0300

    Calc IMC - A26prof

--- Exercicio-A26prof/js/script.js
+++ Exercicio-A26prof/js/script.js
@@ -5,17 +5,50 @@ let pesoInput = document.querySelector('#peso');
 let alturaInput = document.querySelector('#altura');
 
 
+/* Calcula IMC */
+function calculaIMC(peso, altura){
+    return peso / (altura**2);
+}
+
+
+/* Classifica o IMC - (Abaixo do Peso, Normal, Sobrepeso ou Obesidade) */
+function classificacaoIMC(IMC){
+    
+    let p = document.createElement('p');
+    p.innerHTML = `<strong>IMC:</strong> ${IMC.toFixed(2)}`;
+    resultado.appendChild(p);
+
+    if(IMC < 18.5){
+        p.classList.add('abaixoDoPeso');  
+        p.innerHTML += " | Abaixo do peso ideal";
+    }else if(IMC < 24.9){
+        p.classList.add('pesoNormal');
+        p.innerHTML += " | Peso normal";
+    }else if(IMC < 29.9){
+        p.classList.add('sobrepeso');
+        p.innerHTML += " | Você está com sobrepeso";
+    }else{
+        p.classList.add('Obesidade');
+        p.innerHTML += " | Obesidade";
+    }
+
+}
+
+/* Ao clicar em enviar */
 function recebeEventoForm(evento){
     evento.preventDefault();
 
     let peso = parseFloat(pesoInput.value);
     let altura = parseFloat(alturaInput.value);
 
-    let IMC = peso / (altura**2 );
+    const IMC = calculaIMC(peso, altura);
 
-    resultado.innerHTML = `<strong>IMC:</strong> ${IMC.toFixed(2)} <br>`;
-    
+    resultado.innerHTML = "";
+
+    classificacaoIMC(IMC);
+
 }
+
 
 
 form.addEventListener('submit', recebeEventoForm);
